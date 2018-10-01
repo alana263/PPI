096e2ac0689cec25d40c02856edc7c774ad97dd5
20151074010263 <20151074010263@MC113702.ifrn.local>
2018-10-01 08:15:41 -0300

    commit 4

--- agenda-spring/src/agendaspring/controllers/HomeController.java
+++ agenda-spring/src/agendaspring/controllers/HomeController.java
@@ -1,6 +1,13 @@
 package agendaspring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class HomeController {
 
+	@RequestMapping("/")
+	public void homeAlgo() {
+		System.out.println("Chamou o método homeAlgo!");
+	}
 }
